71a28e847794542e42dd1183e5daad7cefcd7c41
unknown <karandhawan2014@gmail.com>
2018-12-11 21:09:53 +0530

    First Program in CPP-Programming

---
+++ Hello_World.cpp
@@ -0,0 +1,16 @@
+/*
+ Cpp Programming
+ Write a code to print "Hello World".
+
+ created 7 Oct 2018
+ by Karan Dhawan
+
+ This example code is in the public domain.
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ cout<<"\"Hello World\"";
+}
